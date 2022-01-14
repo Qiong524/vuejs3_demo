95c14cdf26f906fc19b5b2125bf6b7d3278600e5
Qiong524 <rising4250@gmail.com>
2022-01-14 18:33:13 +0800

    add sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,152 @@
 <template>
-<p>
-<router-link to="/webRTC"> webRTC </router-link>
-</p>
 
-<router-link to="/maskMap"> maskMap </router-link>
-<ul>
-  <li v-for="i in 5" :key="i">
-    <router-link :to="`/users/${i}`"> /users/{{ i }}</router-link>
-  </li>
-</ul>
-<router-view></router-view>
+    <div id="view" :class="[{'collapsed' : collapsed}]">
+      <router-link to="/webRTC"> webRTC </router-link>
+      <router-link to="/maskMap"> maskMap </router-link>
+      <ul>
+        <li v-for="i in 5" :key="i">
+          <router-link :to="`/users/${i}`"> /users/{{ i }}</router-link>
+        </li>
+      </ul>
+      <router-view></router-view>
+    </div>
+    <sidebar-menu
+      class="sidebar"
+      :menu="menu"
+      :collapsed="collapsed"
+      @item-click="onItemClick"
+      @collapse="onCollapse"
+    />
+
 </template>
 
+
+
 <script>
-import { mapActions } from 'vuex';
-import asideMenu from './components/asideMenu.vue';
-import lightBox from './components/lightbox.vue'
-import maskMap from './components/maskMap.vue'
+import { mapActions } from "vuex";
+import { SidebarMenu } from "vue-sidebar-menu";
+import asideMenu from "./components/asideMenu.vue";
+import lightBox from "./components/lightbox.vue";
+import maskMap from "./components/maskMap.vue";
+
+
+import { useI18n } from "vue-i18n";
+// export default {
+//   name: "HomeView",
+//   setup() {
+//     const { t, locale } = useI18n();
+
+//     watch(locale, (newlocale) => {
+//       localStorage.setItem("locale", newlocale);
+//     });
+
+//     return {
+//       t,
+//       locale,
+//     };
+//   },
+
+// };
 
 export default {
-  name: 'App',
+  name: "App",
+  setup() {
+    const { t, locale } = useI18n();
+    return {
+      t,
+      locale,
+    };
+  },  
+  data() {
+    return {
+      menu: [
+        {
+          header: false,
+          title: "",
+        },
+        {
+          href: "/",
+          title: this.t('roomManage'),
+          icon: "fa fa-home",
+        },
+        {
+          href: "/basic-usage",
+          title: this.t('roomMonitor'),
+          icon: "fa fa-bullseye",
+        },
+        {
+          href: "/page",
+          title: "Dropdown Page",
+          icon: "fa fa-list-ul",
+          child: [
+            {
+              href: "/page/sub-page-1",
+              title: "Sub Page 01",
+              icon: "fa fa-file-alt",
+            },
+            {
+              href: "/page/sub-page-2",
+              title: "Sub Page 02",
+              icon: "fa fa-file-alt",
+            },
+          ],
+        },
+      ],
+      collapsed: true,
+    };
+  },
   components: {
+    SidebarMenu,
     asideMenu,
     lightBox,
-    maskMap
+    maskMap,
   },
   methods: {
-    ...mapActions(['fetchLocations', 'fetchPharmacies']),
-    openPopup(id){
+    ...mapActions(["fetchLocations", "fetchPharmacies"]),
+    openPopup(id) {
       this.$refs.map.triggerPopup(id);
-    }
+    },
+    onItemClick(e, i) {
+      console.log("onItemClick");
+    },
+    onCollapse(c) {
+      console.log("onCollapse");
+      this.collapsed = c;
+    },
+    onItemClick(e, i) {
+      console.log("onItemClick");
+    },
+    onCollapse(c) {
+      console.log("onCollapse");
+      this.collapsed = c;
+    },
   },
-  mounted(){
+  mounted() {
     this.fetchLocations();
     this.fetchPharmacies();
-  }
-
-}
+  },
+};
 </script>
 
 <style lang="scss" src="./style.scss"></style>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+#view {
+  padding-left: 350px;
+}
+#view.collapsed {
+  padding-left: 50px;
 }
 
-ul {
-  display: flex;
-  list-style: none;
-  padding: 0;
-  margin: 0 0 1rem 0;
+.sidebar.v-sidebar-menu .vsm-arrow:after {
+  content: "\f105";
+  font-family: "FontAwesome";
 }
-
-li:not(:last-of-type) {
-  margin-right: 1rem;
-}
-
-.router-link-active {
-  color: orange;
-}
-
-.router-link-exact-active {
-  color: crimson;
-}
-
-pre {
-  font-size: 1.1rem;
+.sidebar.v-sidebar-menu .collapse-btn:after {
+  content: "\f07e";
+  font-family: "FontAwesome";
 }
 </style>
+
+
